56c14bbab085f08d66588acc595f96a90a5e4d1e
Nobonita Saha Niha <nobonitasaha042@gmail.com>
2026-06-26 07:36:16 +0600

    Update main and index file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    "Root element not found. Please check index.html and make sure <div id='root'></div> exists."
+  );
+}
+
+createRoot(rootElement).render(<App />);
